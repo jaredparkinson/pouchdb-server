8a3c193f99ccff690387d6e39aa7f8842d9fe359
Jared Parkinson <jared@parkinson.im>
2019-02-09 03:42:50 -0700

    Removed leveldb

--- bin/release.sh
+++ bin/release.sh
@@ -3,11 +3,11 @@
 set -e
 
 if [ ! -z $DRY_RUN ]; then
-  echo "Doing a dry run release..."
-elif [ ! -z $BETA ]; then
-  echo "Doing a beta release to npm..."
+    echo "Doing a dry run release..."
+    elif [ ! -z $BETA ]; then
+    echo "Doing a beta release to npm..."
 else
-  echo "Doing a real release! Use DRY_RUN=1 for a dry run instead."
+    echo "Doing a real release! Use DRY_RUN=1 for a dry run instead."
 fi
 
 #make sure deps are up to date
@@ -27,31 +27,31 @@ node bin/update-package-json-for-publish.js
 
 # Publish all modules with Lerna
 for pkg in $(ls packages/node_modules); do
-  if [ ! -d "packages/node_modules/$pkg" ]; then
-    continue
-  elif [ "true" = $(node --eval "console.log(require('./packages/node_modules/$pkg/package.json').private);") ]; then
-    continue
-  fi
-  cd packages/node_modules/$pkg
-  echo "Publishing $pkg..."
-  if [ ! -z $DRY_RUN ]; then
-    echo "Dry run, not publishing"
-  elif [ ! -z $BETA ]; then
-    npm publish --tag beta
-  else
-    npm publish
-  fi
-  cd -
+    if [ ! -d "packages/node_modules/$pkg" ]; then
+        continue
+        elif [ "true" = $(node --eval "console.log(require('./packages/node_modules/$pkg/package.json').private);") ]; then
+        continue
+    fi
+    cd packages/node_modules/$pkg
+    echo "Publishing $pkg..."
+    if [ ! -z $DRY_RUN ]; then
+        echo "Dry run, not publishing"
+        elif [ ! -z $BETA ]; then
+        npm publish --tag beta
+    else
+        npm publish
+    fi
+    cd -
 done
 
 # Build browser packages
 for pkg in $(ls packages/node_modules); do
-  if [ "false" = $(node --eval "console.log(!!require('./package.json').browserPackages['$pkg']);") ]; then
-      continue
-  fi
-  module_name=$(node --eval "console.log(require('./package.json').browserPackages['$pkg']);")
-  browserify packages/node_modules/$pkg -o packages/node_modules/$pkg/dist/$pkg.js -s $module_name
-  uglifyjs packages/node_modules/$pkg/dist/$pkg.js -o packages/node_modules/$pkg/dist/$pkg.min.js
+    if [ "false" = $(node --eval "console.log(!!require('./package.json').browserPackages['$pkg']);") ]; then
+        continue
+    fi
+    module_name=$(node --eval "console.log(require('./package.json').browserPackages['$pkg']);")
+    browserify packages/node_modules/$pkg -o packages/node_modules/$pkg/dist/$pkg.js -s $module_name
+    uglifyjs packages/node_modules/$pkg/dist/$pkg.js -o packages/node_modules/$pkg/dist/$pkg.min.js
 done
 
 # Create git tag, which is also the Bower/Github release
@@ -60,13 +60,13 @@ git commit -m "build $VERSION"
 
 # Only "publish" to GitHub/Bower if this is a non-beta non-dry run
 if [ -z $DRY_RUN ]; then
- if [ -z $BETA ]; then
-    # Tag and push
-    git tag $VERSION
-    git push --tags git@github.com:pouchdb/pouchdb-server.git $VERSION
-
-    # Cleanup
-    git checkout $SOURCE_DIR
-    git branch -D $BUILD_DIR
-  fi
+    if [ -z $BETA ]; then
+        # Tag and push
+        git tag $VERSION
+        git push --tags git@github.com:pouchdb/pouchdb-server.git $VERSION
+        
+        # Cleanup
+        git checkout $SOURCE_DIR
+        git branch -D $BUILD_DIR
+    fi
 fi
